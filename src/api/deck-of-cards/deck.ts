c3915c9c9abce4fd187c981f3b887e82bb5b7e6f
Cremacious <chrismackall3@gmail.com>
2025-05-28 15:30:46 -0400

    Setup for React Query

--- src/api/deck-of-cards/deck.ts
+++ src/api/deck-of-cards/deck.ts
@@ -1,20 +1,32 @@
-const API_URL = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1';
+const API_URL = 'https://deckofcardsapi.com/api/deck/';
 
-type Deck = {
+export type Deck = {
   success: boolean;
   deck_id: string;
   shuffled: boolean;
   remaining: number;
 };
 
-export async function newDeck(): Promise<Deck | null> {
-  try {
-    const response = await fetch(API_URL);
-    const data: Deck = await response.json();
-    console.log(data);
-    return data;
-  } catch (error) {
-    console.error('Error fetching new deck:', error);
-    return null;
-  }
+export async function fetchNewDeck(): Promise<Deck> {
+  const response = await fetch(`${API_URL}/new/shuffle/?deck_count=1`);
+  if (!response.ok) throw new Error('Network response was not ok');
+  return response.json();
 }
+
+export async function drawCards(deckId: string, count: number) {
+  const response = await fetch(`${API_URL}${deckId}/draw/?count=${count}`);
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return response.json();
+}
+
+export async function shuffleDeck(deckId: string): Promise<Deck> {
+  const response = await fetch(`${API_URL}${deckId}/shuffle/`);
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return response.json();
+}
+
+
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,33 +1,25 @@
-import { useEffect, useState } from 'react';
-import { newDeck } from '../api/deck-of-cards/deck';
+import { useQuery } from '@tanstack/react-query';
+import { fetchNewDeck } from '../api/deck-of-cards/deck';
+import type { Deck } from '../api/deck-of-cards/deck';
 import { Button } from '@/components/ui/button';
 
 const Home = () => {
-  type Deck = {
-    success: boolean;
-    deck_id: string;
-    shuffled: boolean;
-    remaining: number;
-  };
-
-  const [deck, setDeck] = useState<Deck | null>(null);
-
-  useEffect(() => {
-    console.log('1');
-    async function fetchDeck() {
-      const data: Deck | null = await newDeck();
-      if (data !== null && data !== undefined) {
-        setDeck(data);
-        console.log(data);
-      }
-    }
-    fetchDeck();
-  }, []);
+  const {
+    data: deck,
+    isLoading,
+    error,
+    refetch,
+  } = useQuery<Deck>({
+    queryKey: ['newDeck', ],
+    queryFn: fetchNewDeck,
+  });
 
   return (
     <div>
       <h1>Deck of Cards</h1>
-      <Button>Button</Button>
+      <Button onClick={() => refetch()}>New Deck</Button>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Error: {(error as Error).message}</p>}
       <pre>{JSON.stringify(deck, null, 2)}</pre>
     </div>
   );
